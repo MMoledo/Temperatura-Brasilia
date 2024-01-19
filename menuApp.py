f042897e5baea7dbb726bd4ec6a74d6197402eb1
ModalGR-000487 <caio-augusto@live.com>
2024-01-19 13:45:13 -0300

    Add: Selecionar pasta Adicionei a opção de selecionar a pasta (vai ser importante para achar a base de dados depois

--- menuApp.py
+++ menuApp.py
@@ -3,7 +3,11 @@ import PySimpleGUI as sg
 theme = sg.theme('DarkBrown7')
 
 # Define the menu layout
-menu_layout = [['File', ['Open', 'Save', 'Exit']]]
+menu_layout = [['File', ['Open', 'Exit']]]
+
+file_layout = [
+    [sg.Text('Folder'), sg.In(size=(25,1), enable_events=True ,key='-FOLDER-'), sg.FolderBrowse()]
+]
 
 # Create the window layout
 layout = [[sg.Menu(menu_layout)],
@@ -13,7 +17,9 @@ layout = [[sg.Menu(menu_layout)],
            sg.Radio('Anos 80', "filtro", default=False), 
            sg.Radio('Anos 90', "filtro", default=False), 
            sg.Radio('Anos 2000', "filtro", default=False), 
-           sg.Radio('Anos 2010', "filtro", default=False)]
+           sg.Radio('Anos 2010', "filtro", default=False),
+           sg.Radio('Anos 2020', "filtro", default=False)],
+           [sg.Button('Filtrar', size=(10, 1))]
 ]
 
 # Create the window
@@ -24,5 +30,15 @@ while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED or event == 'Exit':
         break
-
+    elif event == 'Open':
+        file_window = sg.Window('Selecionar Pasta', file_layout)
+        while True:
+            file_event, file_values = file_window.read()
+            if file_event == sg.WINDOW_CLOSED or file_event == 'Exit':
+                break
+            elif file_event == '-FOLDER-':
+                folder = file_values['-FOLDER-']
+                print(folder)
+                file_window.close()
+                break
 window.close()
