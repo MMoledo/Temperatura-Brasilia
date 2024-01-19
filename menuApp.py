4f8587a8bac7840631d1b27aeed0b0742089879c
ModalGR-000487 <caio-augusto@live.com>
2024-01-19 12:03:10 -0300

    Add: RadioButtons para fazer o filtro das diferentes decadas

--- menuApp.py
+++ menuApp.py
@@ -3,15 +3,21 @@ import PySimpleGUI as sg
 theme = sg.theme('DarkBrown7')
 
 # Define the menu layout
-menu_layout = [['File', ['Open', 'Save', 'Exit']],
-               ['Edit', ['Cut', 'Copy', 'Paste']]]
+menu_layout = [['File', ['Open', 'Save', 'Exit']]]
 
 # Create the window layout
 layout = [[sg.Menu(menu_layout)],
-          [sg.Text('Hello, world!')]]
+          [sg.Text('Selecione o seu filtro:')],
+          [sg.Radio('Anos 60', "filtro", default=True), 
+           sg.Radio('Anos 70', "filtro", default=False), 
+           sg.Radio('Anos 80', "filtro", default=False), 
+           sg.Radio('Anos 90', "filtro", default=False), 
+           sg.Radio('Anos 2000', "filtro", default=False), 
+           sg.Radio('Anos 2010', "filtro", default=False)]
+]
 
 # Create the window
-window = sg.Window('Menu Example', layout)
+window = sg.Window('Análise de Temperatura', layout)
 
 # Event loop
 while True:
