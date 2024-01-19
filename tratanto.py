ab91135ebd2da1a0e14d059001fc955e14a05db0
Moledo <matheusmoledo@hotmail.com>
2024-01-19 15:34:00 -0300

    add: Tratando arquivos 2000 > 2018

--- tratanto.py
+++ tratanto.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 for arquivo in os.listdir("Data"):
-    df = pd.read_csv("Data/" + arquivo)
+    df = pd.read_csv("Data/" + arquivo,index_col=False,sep=";")
     df.drop(columns=["Pressao","Pressao2","Pressao3","Temperatura Ponto de Orvalho","X","Y","Z","W","Vento","Vento Rajada Maxima"], inplace=True)
     df['Data'] = pd.to_datetime(df['Data'])
     df['Temperatura'] = df['Temperatura'].str.replace(',','.').astype(float)
@@ -11,4 +11,4 @@ for arquivo in os.listdir("Data"):
     df_filtered = df.drop(df[condition].index)
     
     # Salvando as alteracoes
-    df_filtered.to_csv("Data/" + arquivo, index=False)
+    df_filtered.to_csv("Data/" + arquivo, index=False,sep=";")
