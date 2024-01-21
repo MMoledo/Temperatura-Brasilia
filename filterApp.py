a20b1da75bf952addc81436629e42fe72869ed72
Caio Lima <caio.lima@modalgr.com.br>
2024-01-21 11:29:12 -0300

    Fix: Alterei algumas coisas no filtro

--- filterApp.py
+++ filterApp.py
@@ -1,34 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+# import Functions.ipynb as func
 
 def filtrar(caminhoPasta, ano, coluna):
-    base = caminhoPasta + 'DATA_' + ano + '.csv'
+    base = caminhoPasta + 'DATA_' + ano + '.CSV'
     # Caminho do arquivo .csv
 
-'''
-    %run Functions.ipynb
-    # Carregar as funções de organização dos dataframes
-    
-    match coluna:
-        case 'Temperatura':
-            df = tempExat(base)
-        case 'Temperatura Maxima':
-            df = tempMax(base)
-        case 'Temperatura Minima':
-            df = tempMin(base)
-        case 'Umidade do Ar':
-            df = umidadeAr(base)
-        case 'Vento Velocidade':
-            df = velVento(base)
+    df = pd.read_csv(base, sep=';')
 
-    plt.plot(df['Data'], df[f'{coluna}'])
-    plt.xlabel('Data')
-    plt.ylabel(f'{coluna}')
-    plt.title('Variação de ' + coluna + ' em ' + ano)
-    plt.grid(True)
-    # Criação do gráfico
-
-    plt.savefig('assets/img.png')
-    # Salva a imagem gerada
-    '''
+    # func.meanDf(df, ano, coluna)
